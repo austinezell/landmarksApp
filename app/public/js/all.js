51447d430243d93da385f3f6d939929ec0d6748a
Darien Lombardi <darien.lombardi@gmail.com>
2015-10-29 14:25:05 -0700

    update readme

--- app/public/js/all.js
+++ app/public/js/all.js
@@ -74,7 +74,7 @@ angular.module('starter.controllers', []).controller('AppCtrl', function ($scope
   $scope.loginData = {};
 
   // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
+  $ionicModal.fromTemplateUrl('html/login.html', {
     scope: $scope
   }).then(function (modal) {
     $scope.modal = modal;
--- app/src/js/controllers.js
+++ app/src/js/controllers.js
@@ -13,7 +13,7 @@ angular.module('starter.controllers', [])
   $scope.loginData = {};
 
   // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
+  $ionicModal.fromTemplateUrl('html/login.html', {
     scope: $scope
   }).then(function(modal) {
     $scope.modal = modal;
@@ -53,4 +53,5 @@ angular.module('starter.controllers', [])
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
+  
 });
