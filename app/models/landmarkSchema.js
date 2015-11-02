48277fbec01c85aa92e276a50827bd1d81b7de27
austin ezell <austin.r.ezell@gmail.com>
2015-11-02 13:14:07 -0800

    includes more data from webscraper to be loaded. includes an alternative mongodb://localhost if environmental mongodb is not set. expands the Landmark schema.

--- app/models/landmarkSchema.js
+++ app/models/landmarkSchema.js
@@ -5,9 +5,13 @@ var mongoose = require('mongoose');
 var landmarkSchema = new mongoose.Schema({
   name: {type: String, required: true, unique: true},
   location: String,
+  longitude: String,
+  latitude: String,
+  image: String,
   yearOfSignificance: Number,
   blurb: String,
-  stories: [{type: String}]
+  stories: [{type: String}],
+  retistryNumber: Number
 })
 
 module.exports = mongoose.model('Landmark', landmarkSchema)
