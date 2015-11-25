7dbaaee9d3c3e700b7328515ae99f50bf06b653a
Darien Lombardi <darien.lombardi@gmail.com>
2015-11-25 14:44:17 -0800

    sass as last task in default gulp

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ var dirs = {
     js: 'public/js'
   }
 }
-gulp.task('default', ['sass', 'scripts', 'templates']);
+gulp.task('default', ['scripts', 'templates','sass']);
 
 gulp.task('sass', function(done) {
   gulp.src('src/scss/style.scss')
