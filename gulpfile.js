95472eaf6265e90397b97f4e80651a67d7b49e46
Darien Lombardi <darien.lombardi@gmail.com>
2015-11-25 13:36:58 -0800

    remove uninstall gulpfile dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
-var sh = require('shelljs');
 var babel = require('gulp-babel');
 
 
@@ -54,25 +53,4 @@ gulp.task('watch', function() {
   gulp.watch(dirs.src.html, ['templates']);
   gulp.watch(dirs.src.scss, ['sass']);
   gulp.watch(dirs.src.js, ['scripts']);
-});
-
-//
-// gulp.task('install', ['git-check'], function() {
-//   return bower.commands.install()
-//     .on('log', function(data) {
-//       gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-//     });
-// });
-
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
 });
