bce234f135ddad8e0535628485b074a52d40aa90
Austin Ezell <austin.r.ezell@gmail.com>
2016-03-18 14:29:18 -0700

    removes commented out unused code

--- models/userSchema.js
+++ models/userSchema.js
@@ -16,8 +16,6 @@ var UserSchema = new mongoose.Schema({
   badges: []
 })
 
-// unique: true,
-
 UserSchema.methods.setPassword = function(password){
     this.salt = crypto.randomBytes(16).toString('hex');
     this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
